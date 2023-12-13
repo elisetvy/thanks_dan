e11529de074153be331ef1a7a71ddba68ef85b44
Elise Ngo <elisetvy@gmail.com>
2023-12-12 21:39:32 -0800

    Adjust padding

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,7 +1,7 @@
 function Experience() {
 
     return (
-        <div className="HELP px-8 xl:px-36 pt-10 xl:pt-24">
+        <div className="px-8 xl:px-36">
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-10">
                 <div className="w-full aspect-square bg-black"></div>
                 <div className="w-full aspect-square bg-black"></div>
--- src/Landing.jsx
+++ src/Landing.jsx
@@ -2,7 +2,7 @@ function Landing () {
 
     return (
         <div className="px-8 xl:px-36 pt-10 xl:pt-24">
-            <div className="font-black text-5xl leading-[3rem] xl:text-8xl xl:leading-[6rem]">
+            <div className="font-black text-4xl leading-[2.5rem] md:text-5xl md:leading-[3rem] xl:text-8xl xl:leading-[6rem]">
                 <div>Hello.</div>
                 <div>I&apos;m Elise Ngo,</div>
                 <div>a Software Engineer</div>
@@ -10,7 +10,7 @@ function Landing () {
                 <div className="mt-12 lg:mt-24 bg-black w-6 h-0.5"></div>
             </div>
             <div className="mt-6 text-lg max-w-[720px] mb-20 xl:mb-52">
-                I build thoughtfully designed applications that prioritize the user experience. My passion lies in leveraging my multidisciplinary background to approach technical challenges in new and creative ways. Check out some of my work below.
+                I build thoughtfully designed applications that prioritize the user experience. I&apos;m passionate about leveraging my multidisciplinary background to approach technical challenges in new and creative ways. Check out some of my work below.
             </div>
         </div>
     )
