b4a11bf56cc5c77da443499d7018882f071fcbd6
Elise Ngo <elisetvy@gmail.com>
2023-12-12 20:47:22 -0800

    Add to landing

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,11 +1,18 @@
 function Landing () {
 
     return (
-        <div className="px-16 lg:px-36 pt-10 lg:pt-24 font-black text-5xl leading-[3rem] lg:text-8xl lg:leading-[6rem]">
-            <div>Hello.</div>
-            <div>I&apos;m Elise Ngo,</div>
-            <div>a Full Stack Software Engineer based in Long Beach.</div>
-            <div className="mt-12 lg:mt-24 bg-black w-6 h-0.5"></div>
+        <div className="px-8 xl:px-36 pt-10 xl:pt-24">
+            <div className="font-black text-5xl leading-[3rem] xl:text-8xl xl:leading-[6rem]">
+                <div>Hello.</div>
+                <div>I&apos;m Elise Ngo,</div>
+                <div>a Software Engineer</div>
+                <div>based in Long Beach.</div>
+                <div className="mt-12 lg:mt-24 bg-black w-6 h-0.5"></div>
+            </div>
+            <div className="mt-6 text-lg max-w-[720px] mb-20 xl:mb-52">
+                I build thoughtfully designed applications that prioritize the user experience. My passion lies in leveraging my multidisciplinary background to approach technical challenges in new and creative ways. Check out some of my work below.
+            </div>
+            jj
         </div>
     )
 }
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,7 +1,7 @@
 function Nav() {
 
     return (
-        <div className="font-medium text-3xl px-16 lg:px-36 py-8">
+        <div className="font-medium text-2xl px-8 xl:px-36 py-8">
             Elise.
         </div>
     )
