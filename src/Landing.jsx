4c8bc5425ddfd2975c7e2233d4fbd90c3439611d
Elise Ngo <elisetvy@gmail.com>
2023-12-13 16:01:58 -0800

    Add resume

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,4 +1,5 @@
 import Nav from "./Nav";
+import Resume from "./assets/EliseNgo_Resume_December2023.pdf";
 import Experience from "./Experience";
 import Contact from "./Contact";
 
@@ -15,7 +16,11 @@ function Landing () {
                 <div className="mt-12  bg-black w-6 h-0.5"></div>
             </div>
             <div className="mt-12 text-lg max-w-[750px] mb-20 xl:mb-52">
-                I build thoughtfully designed, empathetically engineered applications that prioritize the user experience and developer experience. I&apos;m passionate about leveraging my background in Marketing and Language Studies to approach technical challenges in new and creative ways. Check out some of my work below.
+                I build thoughtfully designed, empathetically engineered applications that prioritize the user experience and developer experience. I&apos;m passionate about leveraging my unique background to approach technical challenges in new and creative ways.
+                <div className="mt-2 flex flex-col sm:flex-row gap-2 ">
+                    <div>Check out some of my work below.</div>
+                    <div onClick={() => window.open(Resume, "_blank")} className="opacity-50 hover:underline hover:underline-offset-4 hover:cursor-pointer">For my resume, click here.</div>
+                </div>
             </div>
             <Experience />
             <Contact />
