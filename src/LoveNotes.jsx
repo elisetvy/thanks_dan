a387dc039c7919a865ecc0d0a6dd70aa6472d694
Elise Ngo <elisetvy@gmail.com>
2023-12-14 19:54:52 -0800

    Update description

--- src/LoveNotes.jsx
+++ src/LoveNotes.jsx
@@ -12,7 +12,7 @@ function LoveNotes() {
             <img className="mt-10 border w-full" src={LoveNotes_1} alt="love notes" />
             <div className="py-10">
                 <div className="text-3xl font-black">Love Notes</div>
-                <div className="mt-4 text-lg">Enhanced client-side error-handling, incorporated AWS S3 for the secure storage of user-uploaded images, and expanded test coverage.</div>
+                <div className="mt-4 text-lg">Enhanced client-side error handling, incorporated AWS S3 for the secure storage of user-uploaded images, and expanded test coverage.</div>
                 <div className="mt-4 text-md font-black tracking-widest opacity-25">TECHNOLOGIES USED</div>
                 <div className="text-lg">JavaScript, React, TailwindCSS, Node/Express, JSON Schema, JSON Web Tokens, PostgreSQL, MapQuest, AWS S3, Jest, SuperTest, Git, GitHub</div>
             </div>
