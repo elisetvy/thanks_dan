f2e7b9404b21dba3fa58c43cb4022b8fc0e0ce1a
Elise Ngo <elisetvy@gmail.com>
2024-01-03 15:56:07 -0800

    Add opacity/translateY to the transition/animation

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -16,19 +16,37 @@ useEffect(() => {
     return (
         <div className="px-8 xl:px-36 pt-10">
             <Nav />
-            <CSSTransition
-                in={fadeIn}
-                timeout={3000}
-                classNames="fade-in"
-            >
-                <div className="pt-12 xl:pt-32 font-black text-4xl leading-[2.5rem] md:text-5xl md:leading-[3rem] xl:text-8xl xl:leading-[6rem]">
+            <div className="pt-12 xl:pt-32 font-black text-4xl leading-[2.5rem] md:text-5xl md:leading-[3rem] xl:text-8xl xl:leading-[6rem]">
+                <CSSTransition
+                    in={fadeIn}
+                    timeout={1000}
+                    classNames="fade-in-1"
+                >
                     <div>Hello.</div>
+                </CSSTransition>
+                <CSSTransition
+                    in={fadeIn}
+                    timeout={1000}
+                    classNames="fade-in-2"
+                >
                     <div>I&apos;m Elise Ngo,</div>
-                    <div>a Software Engineer</div>
+                </CSSTransition>
+                <CSSTransition
+                    in={fadeIn}
+                    timeout={1000}
+                    classNames="fade-in-3"
+                >
+                    <div>a Software Engineer.</div>
+                </CSSTransition>
+                <CSSTransition
+                    in={fadeIn}
+                    timeout={1000}
+                    classNames="fade-in-4"
+                >
                     <div>based in Long Beach.</div>
-                    <div className="mt-12  bg-black w-6 h-0.5"></div>
-                </div>
-            </CSSTransition>
+                </CSSTransition>
+                <div className="mt-12  bg-black w-6 h-0.5"></div>
+            </div>
             <div className="animate__delay-5s mt-12 text-lg max-w-[750px] mb-20 xl:mb-52">
                 I build thoughtfully designed, empathetically engineered applications that prioritize the user and developer experience. I&apos;m passionate about leveraging my unique background to approach technical challenges in new and creative ways.
                 <div className="mt-2 flex flex-col sm:flex-row gap-2 ">
