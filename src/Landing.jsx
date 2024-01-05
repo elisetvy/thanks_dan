6cd5773b06c0bda8cc98649ca4f6ff9a00b65835
Elise Ngo <elisetvy@gmail.com>
2024-01-05 01:31:50 -0800

    Update resume

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { CSSTransition } from "react-transition-group";
 
 import Nav from "./Nav";
-import Resume from "./assets/EliseNgo_Resume_December2023.pdf";
+import Resume from "./assets/EliseNgo_Resume_January2024.pdf";
 import Experience from "./Experience";
 import Contact from "./Contact";
 
